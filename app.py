f9820b0cd6a2d2e6d2255632956c4b268fe6780d
cwverica <cwverica@gmail.com>
2021-04-26 10:56:43 -0700

    another attempt

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL?sslmode=require', 'postgresql:///user_auth')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL?sslmode=require', 'postgresql:///user_auth').replace('postgres://', 'postgresql://')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
